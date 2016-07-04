8ded234d1e0fa47db549f467b682b41d637a624e
Vízy <kristof.vizy@gmail.com>
2016-07-04 23:28:04 +0200

    video

--- js/script.js
+++ js/script.js
@@ -40,5 +40,10 @@ function loadWeather(location, woeid) {
 }
 
 
-
+$(document).ready(function () {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#hotels').offset().top
+    }, 'slow');
+});
 
